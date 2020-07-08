08ca938fa2c53067a1aa054ae74cafa450144cfc
Panitarn Jongprasoplarp <panitarnjong@gmail.com>
2020-07-08 21:23:29 +0700

    Add project files.

---
+++ MiraiiWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MiraiiWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
